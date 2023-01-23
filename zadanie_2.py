8048bdb174dec8cf887d9d7adbf6be1d561a7abf
Marat <brutto061@gmail.com>
2023-01-23 20:57:03 +0700

    попрвили код

--- zadanie_2.py
+++ zadanie_2.py
@@ -18,37 +18,38 @@ def check_win():
         if cell[item[0]] == cell[item[1]] == cell[item[2]]:
             return cell[item[0]]
 
-def check_line(sum_O, sum_X):
+def check_move(numO, numX):
     move = ''
     for item in win_line:
-        o = 0
-        x = 0
-        for j in range(3):
-            if cell[item[j]] == 'O':
-                o += 1
-            if cell[item[j]] == 'X':
-                x += 1
-        if o == sum_O and x == sum_X:
-            for j in range(3):
-                if cell[item[j]] != 'O' and cell[item[j]] != 'X':
-                    move = cell[item[j]]
+        count_o = 0 
+        count_x = 0
+        for i in range(3):
+            if cell[item[i]] == 'O':
+                count_o += 1
+            if cell[item[i]] == 'X':
+                count_x += 1
+        if count_o == numO and count_x == numX:
+            for i in range(3):
+                if cell[item[i]] != 'O' and cell[item[i]] != 'X':
+                    move = cell[item[i]]
     return move
 
 def comp():        
     move = ''
-    move = check_line(2,0)
+    move = check_move(2,0)
     
     if move == '':
-        move = check_line(0,2)        
+        move = check_move(0,2)        
  
     if move == '':
-        move = check_line(1,0)           
+        move = check_move(1,0)  
+
+    if move == '':
+        move = check_move(1,1)         
     
     if move == '':
         if cell[4] != 'X' and cell[4] != 'O':
-            move = 5           
- 
-    if move == '':
+            move = 5
         if cell[0] != 'X' and cell[0] != 'O':
             move = 1           
     return move
@@ -73,12 +74,21 @@ while not game_over:
             create_cell(cell)
             print(f'Победили {win}!!!')
             game_over = True
-            break
+        
     if count ==9:
         create_cell(cell)
         print('Ничья!!!')
         game_over = True
         
+ 
+
+    
+
+
+
+
+
+
  
 
     
